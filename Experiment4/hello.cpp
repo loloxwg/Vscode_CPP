3f671015c5952a927d234dcf74017b8cc525f6d0
loloxwg <loloxwg@outlook.com>
2020-11-27 14:38:18 +0800

    hh

--- Experiment4/hello.cpp
+++ Experiment4/hello.cpp
@@ -3,3 +3,31 @@
 int main(){
     printf("hello");
 }
+
+
+//先序创建二叉树
+// int CreateBiTree(BiTree *T)
+// {
+//     ElemType ch;
+//     ElemType temp;
+//     scanf("%d", &ch);
+//     temp = getchar();
+//     if (ch == -1)
+//     {
+//         *T = NULL;
+//     }
+//     else
+//     {
+//         *T = (BiTree)malloc(sizeof(BiTNode));
+//         if (!(*T))
+//         {
+//             exit(-1);
+//         }
+//         (*T)->data = ch;
+//         printf("输入%d的左子结点：", ch);
+//         CreateBiTree(&(*T)->lChild);
+//         printf("输入%d的右子结点：", ch);
+//         CreateBiTree(&(*T)->rChild);
+//     }
+//     return 1;
+// }
