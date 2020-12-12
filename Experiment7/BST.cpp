0af7a9db6d654011bcc8dbaaaa2dd665fcf030f1
loloxwg <loloxwg@outlook.com>
2020-12-12 15:52:56 +0800

    BST,二叉排序树

---
+++ Experiment7/BST.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+#define Status int
+typedef struct BiTNode
+{
+    int data;
+    struct BiTNode *lchild,*richild;
+}BiTNode,*BiTree;
+
+Status SearchBST (BiTNode T,int key,BiTNode f,BiTree *p){
+    
+}
+
